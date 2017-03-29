f71ad07523440eb9a66f940fcecc44d1255686d6
SimplyWorld <szhilin87@gmail.com>
2017-03-29 22:20:37 +0300

    make/edit Cart part 4

--- models/Cart.php
+++ models/Cart.php
@@ -30,4 +30,14 @@ class Cart extends ActiveRecord
         $_SESSION['cart.sum'] = isset($_SESSION['cart.sum']) ? $_SESSION['cart.sum'] + $qty * $auto->price :
                                                                                        $qty * $auto->price;
     }
+
+    public function recalc($id)
+    {
+        if(!isset($_SESSION['cart'][$id])) return false;
+        $qtyMinus = $_SESSION['cart'][$id]['qty'];
+        $sumMinus = $_SESSION['cart'][$id]['qty'] * $_SESSION['cart'][$id]['price'];
+        $_SESSION['cart.qty'] -= $qtyMinus;
+        $_SESSION['cart.sum'] -= $sumMinus;
+        unset($_SESSION['cart'][$id]);
+    }
 }
